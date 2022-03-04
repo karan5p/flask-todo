ab50d85b1e6de0d8bdded436a564a4c9ff5c5a61
Karan Patel <karancpatel5@gmail.com>
2022-03-03 22:51:00 -0500

    Add functionality added + SQLite database

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+    # def __init__(self, title, complete=False):
+    #     self.title = title
+    #     self.complete = complete
 
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    # show all todos
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
 
 
-@app.route('/about')
-def about():
-    return "About page"
+@app.route('/add', methods=['POST'])
+def add():
+    # add new item
+    title = request.form.get('title')
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
